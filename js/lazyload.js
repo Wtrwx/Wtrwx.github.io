1edb42cf746ee552e6e60ff8836e7c7c8fa17f3d
Wtrwx <t156156@qq.com>
2020-03-31 23:46:40 +0800

    Site updated: 2020-03-31 23:46:39

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 31 2020 23:44:29 GMT+0800 (中国标准时间)
+// build time:Tue Mar 31 2020 23:46:38 GMT+0800 (中国标准时间)
 (function(t){var n=Array.prototype.slice.call(document.querySelectorAll("img[srcset]"));function e(n){var e=n.getBoundingClientRect();var r=t.innerHeight||document.documentElement.clientHeight;return e.top>=0&&e.left>=0&&e.top<=r*3}function r(t,n){var e=new Image,r=t.getAttribute("src");e.onload=function(){t.srcset=r;n?n():null};e.srcset=r}function i(){for(var i=0;i<n.length;i++){if(e(n[i])){(function(t){var e=n[t];r(e,function(){n=n.filter(function(t){return e!==t})})})(i)}}if(n.length===0){t.removeEventListener("scroll",c)}}function o(t,n){clearTimeout(t.tId);t.tId=setTimeout(function(){t.call(n)},100)}var c=function(){o(i,t)};i();t.addEventListener("scroll",c)})(this);
 //rebuild by neat 
