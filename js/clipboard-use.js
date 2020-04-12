0dff2e60350c556a7b20a12c86df32aadb2d6bfe
Wtrwx <t156156@qq.com>
2020-04-12 18:29:05 +0800

    Site updated: 2020-04-12 18:28:55

--- js/clipboard-use.js
+++ js/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 12 2020 11:58:00 GMT+0800 (中国标准时间)
+// build time:Sun Apr 12 2020 18:28:53 GMT+0800 (中国标准时间)
 !function(n,o,t){var i=function(){var n="";n+='<button class="copy-btn" data-clipboard-snippet="">';n+='<i class="iconfont iconcopy"></i><span>Copy</span>';n+="</button>";$("pre.highlight").wrap($('<div class="code-block"></div>'));$(".code-block").prepend(n);var o=new ClipboardJS(".copy-btn",{target:function(n){return n.nextElementSibling}});o.on("success",function(o){showSnackbar("复制成功");setTimeout(function(){o.trigger.outerHTML=n},2e3)})};var c=window.onload;window.onload=function(){i();c&&c()}}(window,document);
 //rebuild by neat 
